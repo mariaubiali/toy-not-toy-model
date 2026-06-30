b7827648363aa1d6c21b7d3fa7258df4867f5877
Nina Elmer <nelmer@Ninas-MacBook-Pro-2.local>
2026-06-30 12:09:20 +0100

    Cleaning up the repo, adding new conda env files

--- T3gp/chi2/run_nc_ep.sh
+++ T3gp/chi2/run_nc_ep.sh
@@ -5,13 +5,13 @@ HERA="../data/out/hera_nc_ep_575.npz"
 OUT="out/chi2_result.npz"
 
 MODELS=(
-  "../results/nn/chi2_th_full/nn_summary.npz"
-  "../results/nn/mse_th_full/nn_summary.npz"
-  "../results/nn/chi2_full/nn_summary.npz"
-  "../results/nn/mse_full/nn_summary.npz"
-  "../results/ntk/init/nn_summary.npz"
-  "../results/ntk/post_full/nn_summary.npz"
-  "../results/gp/rbf_full3/gp_summary.npz"
+  # "../results/nn/chi2_th_full/nn_summary.npz"
+  # "../results/nn/mse_th_full/nn_summary.npz"
+  # "../results/nn/chi2_full/nn_summary.npz"
+  # "../results/nn/mse_full/nn_summary.npz"
+  # "../results/ntk/init/nn_summary.npz"
+  # "../results/ntk/post_full/nn_summary.npz"
+  # "../results/gp/rbf_full3/gp_summary.npz"
 )
 
 for model in "${MODELS[@]}"; do
